dd49a10c2cf64373d5b6fab562a1aa5d956b2eda
SamedhG <gupta.sam@husky.neu.edu>
2019-12-17 19:45:54 +0530

    formatting

--- src/day1.rs
+++ src/day1.rs
@@ -3,7 +3,7 @@ use std::io::prelude::*;
 use std::iter::Iterator;
 
 fn calculate_fuel(f: i64) -> i64 {
-    f/3 - 2
+    f / 3 - 2
 }
 
 fn cumulative_fuel(f: i64) -> i64 {
@@ -14,9 +14,9 @@ fn cumulative_fuel(f: i64) -> i64 {
     }
 }
 
-fn get_input() ->  Box<Vec<i64>> { 
+fn get_input() -> Box<Vec<i64>> {
     let mut file = File::open("resources/day1.txt").unwrap();
-    let mut input =  String::new();
+    let mut input = String::new();
     file.read_to_string(&mut input).unwrap();
     let iter = input.split_whitespace();
     Box::new(iter.map(|x| x.parse::<i64>().unwrap()).collect())
@@ -29,7 +29,8 @@ pub fn p1() -> i64 {
 
 pub fn p2() -> i64 {
     let arr = get_input();
-    arr.iter().fold(0, |acc, x| acc + cumulative_fuel(calculate_fuel(*x)))
+    arr.iter()
+        .fold(0, |acc, x| acc + cumulative_fuel(calculate_fuel(*x)))
 }
 
 #[cfg(test)]
