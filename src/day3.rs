bbbfe1a5e7dc6207cbb25a2053c38f81d5347f22
SamedhG <gupta.sam@husky.neu.edu>
2019-12-18 09:44:10 +0530

    Added solution for day3 part2
    This solution seems less than ideal especially seing the number of clones
    needed for it to work.

--- src/day3.rs
+++ src/day3.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum Direction {
     Left,
     Right,
@@ -10,7 +10,7 @@ enum Direction {
     Down,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Wire(Direction, usize);
 
 fn convert(s: &str) -> Box<Wire> {
@@ -66,6 +66,53 @@ fn manhattan_distance(point: (i64, i64)) -> i64 {
     x.abs() + y.abs()
 }
 
+fn steps_to_reach(point: (i64, i64), wires: Box<Vec<Box<Wire>>>) -> i64 {
+    let mut x = 0;
+    let mut y = 0;
+    let mut steps = 0;
+    for wire in *wires {
+        match (*wire).0 {
+            Direction::Right => {
+                for _ in 0..wire.1 {
+                    x += 1;
+                    steps+=1;
+                    if (x,y) == point {
+                        return steps;
+                    }
+                }
+            }
+            Direction::Left => {
+                for _ in 0..wire.1 {
+                    x -= 1;
+                    steps+=1;
+                    if (x,y) == point {
+                        return steps;
+                    }
+                }
+            }
+            Direction::Up => {
+                for _ in 0..wire.1 {
+                    y += 1;
+                    steps+=1;
+                    if (x,y) == point {
+                        return steps;
+                    }
+                }
+            }
+            Direction::Down => {
+                for _ in 0..wire.1 {
+                    y -= 1;
+                    steps+=1;
+                    if (x,y) == point {
+                        return steps;
+                    }
+                }
+            }
+        }
+    }
+    panic!("Wire doesnt reach that point")
+}
+
 fn get_input() -> (Box<Vec<Box<Wire>>>, Box<Vec<Box<Wire>>>) {
     let mut file = File::open("resources/day3.txt").unwrap();
     let mut input = String::new();
@@ -80,11 +127,26 @@ fn get_input() -> (Box<Vec<Box<Wire>>>, Box<Vec<Box<Wire>>>) {
 
 pub fn p1() -> i64 {
     let input = get_input();
-    let wire1 = find_coords(input.0);
-    let wire2 = find_coords(input.1);
-    wire1
-        .intersection(&wire2)
+    let wire1 = input.0;
+    let wire2 = input.1;
+    let wire1_points = find_coords(wire1);
+    let wire2_points = find_coords(wire2);
+    wire1_points
+        .intersection(&wire2_points)
         .map(|x| manhattan_distance(*x))
+        .min()
+        .unwrap()
+}
+
+pub fn p2() -> i64 {
+    let input = get_input().to_owned();
+    let wire1 = input.0.clone();
+    let wire2 = input.1.clone();
+    let wire1_points = find_coords(input.0);
+    let wire2_points = find_coords(input.1);
+    wire1_points
+        .intersection(&wire2_points)
+        .map(|x| steps_to_reach(*x, wire1.clone()) + steps_to_reach(*x, wire2.clone()))
         .min()
         .unwrap()
 }
@@ -97,5 +159,10 @@ mod tests {
     #[test]
     fn test_p1() {
         assert_eq!(p1(), 209);
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(p2(), 43258);
     }
 }
