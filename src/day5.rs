7cec08fe359b0fc3bc8c1243757540d41244b38e
SamedhG <gupta.sam@husky.neu.edu>
2019-12-19 09:13:09 +0530

    Aded implementation for D5P2

--- src/day5.rs
+++ src/day5.rs
@@ -6,6 +6,10 @@ enum Opcode {
     Multiply(i64, i64, i64),
     Input,
     Output,
+    JumpIfTrue(i64, i64),
+    JumpIfFalse(i64, i64),
+    LessThan(i64, i64, i64),
+    Equals(i64, i64, i64),
     Halt,
 }
 
@@ -23,6 +27,24 @@ fn parse_opcode(opcode: i64) -> Opcode {
         ),
         3 => Opcode::Input,
         4 => Opcode::Output,
+        5 => Opcode::JumpIfTrue(
+            (opcode / 100) % 10,
+            (opcode / 1000) % 10,
+        ),
+        6 => Opcode::JumpIfFalse(
+            (opcode / 100) % 10,
+            (opcode / 1000) % 10,
+        ),
+        7 => Opcode::LessThan(
+            (opcode / 100) % 10,
+            (opcode / 1000) % 10,
+            (opcode / 10000) % 10,
+        ),
+        8 => Opcode::Equals(
+            (opcode / 100) % 10,
+            (opcode / 1000) % 10,
+            (opcode / 10000) % 10,
+        ),
         99 => Opcode::Halt,
         _ => panic!("Unknown Opcode"),
     }
@@ -36,40 +58,16 @@ fn intcode(memory: &mut Vec<i64>, input: &mut Vec<i64>, output: &mut Vec<i64>) {
         }
         match parse_opcode(memory[x]) {
             Opcode::Add(m1, m2, m3) => {
-                let arg_1 = if m1 == 0 {
-                    memory[x + 1] as usize
-                } else {
-                    x + 1
-                };
-                let arg_2 = if m2 == 0 {
-                    memory[x + 2] as usize
-                } else {
-                    x + 2
-                };
-                let update_index = if m3 == 0 {
-                    memory[x + 3] as usize
-                } else {
-                    x + 3
-                };
+                let arg_1 = if m1 == 0 { memory[x + 1] as usize } else { x + 1 };
+                let arg_2 = if m2 == 0 { memory[x + 2] as usize } else { x + 2 };
+                let update_index = if m3 == 0 { memory[x + 3] as usize } else { x + 3 };
                 memory[update_index] = memory[arg_1] + memory[arg_2];
                 x += 4;
             }
             Opcode::Multiply(m1, m2, m3) => {
-                let arg_1 = if m1 == 0 {
-                    memory[x + 1] as usize
-                } else {
-                    x + 1
-                };
-                let arg_2 = if m2 == 0 {
-                    memory[x + 2] as usize
-                } else {
-                    x + 2
-                };
-                let update_index = if m3 == 0 {
-                    memory[x + 3] as usize
-                } else {
-                    x + 3
-                };
+                let arg_1 = if m1 == 0 { memory[x + 1] as usize } else { x + 1 };
+                let arg_2 = if m2 == 0 { memory[x + 2] as usize } else { x + 2 };
+                let update_index = if m3 == 0 { memory[x + 3] as usize } else { x + 3 };
                 memory[update_index] = memory[arg_1] * memory[arg_2];
                 x += 4;
             }
@@ -82,6 +80,32 @@ fn intcode(memory: &mut Vec<i64>, input: &mut Vec<i64>, output: &mut Vec<i64>) {
                 let loc = memory[x + 1] as usize;
                 output.push(memory[loc]);
                 x += 2;
+            }
+            Opcode::JumpIfTrue(m1, m2) => {
+                let check = if m1 == 0 { memory[x + 1] as usize } else { x + 1 };
+                let new_p = if m2 == 0 { memory[x + 2] as usize } else { x + 2 };
+                if memory[check] != 0 { x = memory[new_p] as usize } else { x+= 3 }
+            }
+            Opcode::JumpIfFalse(m1, m2) => {
+                let check = if m1 == 0 { memory[x + 1] as usize } else { x + 1 };
+                let new_p = if m2 == 0 { memory[x + 2] as usize } else { x + 2 };
+                if memory[check] == 0 { x = memory[new_p] as usize } else { x+= 3 }
+            }
+            Opcode::LessThan(m1, m2, m3) => {
+                let arg_1 = if m1 == 0 { memory[x + 1] as usize } else { x + 1 };
+                let arg_2 = if m2 == 0 { memory[x + 2] as usize } else { x + 2 };
+                let update_index = if m3 == 0 { memory[x + 3] as usize } else { x + 3 };
+                let to_set = if memory[arg_1] < memory[arg_2] { 1 } else { 0 };
+                memory[update_index] = to_set;
+                x += 4;
+            }
+            Opcode::Equals(m1, m2, m3) => {
+                let arg_1 = if m1 == 0 { memory[x + 1] as usize } else { x + 1 };
+                let arg_2 = if m2 == 0 { memory[x + 2] as usize } else { x + 2 };
+                let update_index = if m3 == 0 { memory[x + 3] as usize } else { x + 3 };
+                let to_set = if memory[arg_1] == memory[arg_2] { 1 } else { 0 };
+                memory[update_index] = to_set;
+                x += 4;
             }
             Opcode::Halt => {
                 break;
@@ -103,6 +127,14 @@ pub fn p1() -> i64 {
     let mut input = vec![1];
     let mut output = Vec::new();
     intcode(&mut *memory, &mut input, &mut output);
+    output[output.len() - 1]
+}
+
+pub fn p2() -> i64 {
+    let mut memory = get_input();
+    let mut input = vec![5];
+    let mut output = Vec::new();
+    intcode(&mut *memory, &mut input, &mut output);
     println!("{:?}", output);
     output[output.len() - 1]
 }
@@ -115,5 +147,10 @@ mod tests {
     #[test]
     fn test_p1() {
         assert_eq!(p1(), 11049715);
+    }
+
+    #[test]
+    fn test_p2() {
+        assert_eq!(p2(), 2140710);
     }
 }
