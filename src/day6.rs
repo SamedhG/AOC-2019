33804023dbf07951ae085d6180b7a96e865a92e9
SamedhG <gupta.sam@husky.neu.edu>
2019-12-20 07:54:01 +0530

    Aded implementation for D6P2

--- src/day6.rs
+++ src/day6.rs
@@ -1,9 +1,9 @@
-use std::collections::HashSet;
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::prelude::*;
 
-
+// This assumes that three are no cycles
 fn count_orbits(map: &HashMap<String, String>, body: &String) -> usize {
     match (*map).get(body) {
         None => 0,
@@ -11,15 +11,31 @@ fn count_orbits(map: &HashMap<String, String>, body: &String) -> usize {
     }
 }
 
+// This assumes that there are no cycles
+fn get_chain(map: &HashMap<String, String>, body: &String) -> Vec<String> {
+    let mut ancestors = Vec::new();
+    let mut curr = body;
+    loop {
+        match (*map).get(curr) {
+            None => break,
+            Some(x) => {
+                curr = x;
+                ancestors.push(x.clone());
+            }
+        }
+    }
+    ancestors
+}
+
 // Returns a child->parent map and a set of all the bodies
-fn get_input() -> (HashMap<String, String>, HashSet<String>){
+fn get_input() -> (HashMap<String, String>, HashSet<String>) {
     let mut file = File::open("resources/day6.txt").unwrap();
     let mut input = String::new();
     file.read_to_string(&mut input).unwrap();
     let mut map = HashMap::new();
     let mut set = HashSet::new();
     for line in input.lines() {
-        let orbit : Vec<String> = line.split(')').map(|x| String::from(x)).collect();
+        let orbit: Vec<String> = line.split(')').map(|x| String::from(x)).collect();
         map.insert(orbit[1].clone(), orbit[0].clone());
         set.insert(orbit[0].clone());
         set.insert(orbit[1].clone());
@@ -35,9 +51,24 @@ pub fn p1() -> usize {
     total
 }
 
+pub fn p2() -> usize {
+    let (map, _) = get_input();
+    let my_anc = get_chain(&map, &String::from("YOU"));
+    let santa_anc = get_chain(&map, &String::from("SAN"));
+    let mut dist_to_santa = 0;
+    for ancestor in my_anc {
+        dist_to_santa += 1;
+        match santa_anc.iter().position(|x| *x == ancestor) {
+            None => continue,
+            Some(x) => return x + dist_to_santa - 1,
+        }
+    }
+    panic!("no cmmon ancestor found");
+}
+
 #[cfg(test)]
 mod tests {
-    
+
     use super::*;
 
     #[test]
@@ -45,4 +76,8 @@ mod tests {
         assert_eq!(p1(), 268504);
     }
 
+    #[test]
+    fn test_p2() {
+        assert_eq!(p2(), 409);
+    }
 }
